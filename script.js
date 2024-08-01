47cbc9c2677214d580d87b9ffb54838afac8206a
Ashwini277 <devarushiashwini@gmail.com>
2024-08-01 20:21:24 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-describe('Table 1 Tests', () => {
-    it('checks table classes', () => {
-        cy.get('#table-1').should('be.visible').should('have.class', 'middle');
-        cy.get('#t12').should('have.class', 'center');
-        cy.get('#t13').should('have.class', 'right');
-    });
-});
-
